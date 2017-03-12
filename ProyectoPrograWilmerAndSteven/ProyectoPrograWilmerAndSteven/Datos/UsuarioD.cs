2f4552e37ed7d9b8519828c1b304bb5e553a6997
vvilm <vvilmervd@gmail.com>
2017-03-11 19:38:54 -0600

    terminacion de la clase UsuarioE y clase usuarioD

---
+++ ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Datos/UsuarioD.cs
@@ -0,0 +1,63 @@
+﻿using Datos;
+using Logica;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPrograWilmerAndSteven.Datos
+{
+    public class UsuarioD
+    {
+        private AccesoDatosPostgre conexion;
+
+        private bool error;
+        public bool Error
+        {
+            get { return error; }
+        }
+
+        private string errorMsg;
+        public string ErrorMsg
+        {
+            get { return errorMsg; }
+        }
+
+        public UsuarioD()
+        {
+            this.conexion = AccesoDatosPostgre.Instance;
+            this.limpiarError();
+        }
+
+        public void limpiarError()
+        {
+            this.error = false;
+            this.errorMsg = "";
+        }
+
+        public List<UsuarioE> obtenerUsuarios()
+        {
+
+            this.limpiarError();
+            List<UsuarioE> usuarios = new List<UsuarioE>();
+            DataSet dsetUsuarios;
+            string sql = "select u.id_login as login, u.contrasenia as contrasenia, " +
+                        " u.administrador as administrador, u.sistema as sistema," +
+                        "u.parametros as parametros, u.administracion_de_ordenes as administracionOrdenes," +
+                        "u.gestion_gerencial as gestionGerencial"+
+                         "from Usuario u";
+
+            dsetUsuarios = this.conexion.ejecutarConsultaSQL(sql);
+            foreach (DataRow tupla in dsetUsuarios.Tables[0].Rows)
+            {
+                UsuarioE oUsuario = new UsuarioE(tupla["login"].ToString(),tupla["contrasenia"].ToString(),Convert.ToBoolean(tupla["administrador"].ToString()),
+                    Convert.ToBoolean(tupla["sistema"].ToString()),Convert.ToBoolean(tupla["parametros"].ToString()),Convert.ToBoolean(tupla["administracionOrdenes"].ToString()),
+                    Convert.ToBoolean(tupla["gestionGerencial"].ToString()));
+                usuarios.Add(oUsuario);
+            }
+            return usuarios;
+        }
+    }
+}
--- ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Logica/UsuarioE.cs
+++ ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Logica/UsuarioE.cs
@@ -8,6 +8,120 @@ namespace Logica
 {
     public class UsuarioE
     {
+        private string login;
+        private string contrasenia;
+        private Boolean administrador;
+        private Boolean sistema;
+        private Boolean parametros;
+        private Boolean administracionDeOrdenes;
+        private Boolean gestionGerencial;
 
+
+        public UsuarioE(string pLogin, string pContrasennia,Boolean pAdministrador, Boolean pSistema, Boolean pParametros, Boolean pAdminisitracion, Boolean pGestionGerencial)
+        {
+            this.login = pLogin;
+            this.contrasenia = pContrasennia;
+            this.administrador = pAdministrador;
+            this.sistema = pSistema;
+            this.parametros = pParametros;
+            this.administracionDeOrdenes = pAdminisitracion;
+            this.gestionGerencial = pGestionGerencial;
+        }
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string Contrasenia
+        {
+            get
+            {
+                return contrasenia;
+            }
+
+            set
+            {
+                contrasenia = value;
+            }
+        }
+
+        public bool Sistema
+        {
+            get
+            {
+                return sistema;
+            }
+
+            set
+            {
+                sistema = value;
+            }
+        }
+
+        public bool Parametros
+        {
+            get
+            {
+                return parametros;
+            }
+
+            set
+            {
+                parametros = value;
+            }
+        }
+
+        public bool AdministracionDeOrdenes
+        {
+            get
+            {
+                return administracionDeOrdenes;
+            }
+
+            set
+            {
+                administracionDeOrdenes = value;
+            }
+        }
+
+        public bool GestionGerencial
+        {
+            get
+            {
+                return gestionGerencial;
+            }
+
+            set
+            {
+                gestionGerencial = value;
+            }
+        }
+
+        public bool Administrador
+        {
+            get
+            {
+                return administrador;
+            }
+
+            set
+            {
+                administrador = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return (this.login + " " + this.contrasenia + " " + this.administrador + " " + this.sistema + " " +
+                this.parametros + " " + this.administracionDeOrdenes + " " + this.gestionGerencial);
+        }
     }
 }
