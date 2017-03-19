a4dfc7c6fb7a5aa9a8d8176d842bfe91ef165c0f
vvilm <vvilm@MSI>
2017-03-19 13:44:10 -0600

    FrmCatalogoRepuestos terminados, falta terminar los D

--- ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmCatalogoDeRepuesto.cs
+++ ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmCatalogoDeRepuesto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Logica;
+using ProyectoPrograWilmerAndSteven.Datos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,105 @@ namespace ProyectoPrograWilmerAndSteven.Vista
 {
     public partial class FrmCatalogoDeRepuesto : Form
     {
+        CatalogoRepuestoD oCatalogoRepuestoD = new CatalogoRepuestoD();
         public FrmCatalogoDeRepuesto()
         {
             InitializeComponent();
+            this.CargarDGview();
+        }
+
+        private void CargarDGview()
+        {
+            List<CatalogoRepuestoE> catalogoRepuesto = oCatalogoRepuestoD.obtenerCatalogoRepuestos();
+            if (!oCatalogoRepuestoD.Error)
+            {
+                this.dGVCatalogoRepuestos.DataSource = catalogoRepuesto;
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            //FrmRegistroDeCatalogoDeRepuestos oFrm = new FrmRegistroDeCatalogoDeRepuestos();
+            //oFrm.ShowDialog();
+
+            //if (oFrm.aceptar)
+            //{
+            //    if (oCatalogoRepuestoD.agregarCatalogoRepuesto(oFrm.oCatalogoRepuestoE))
+            //    {
+            //        this.CargarDGview();
+            //        MessageBox.Show("Catalogo agregado");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Error al agregar Catalogo: " +
+            //                   oCatalogoRepuestoD.ErrorMsg, "Error",
+            //                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+            //}
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            //if (this.dGVCatalogoRepuestos.Rows.Count > 0)
+            //{
+
+            //    int fila = this.dGVCatalogoRepuestos.CurrentRow.Index;
+
+            //    CatalogoRepuestoE oCatalogoRepuestoE = new CatalogoRepuestoE(Convert.ToInt32(this.dGVCatalogoRepuestos[0, fila].Value.ToString()),
+            //                             this.dGVCatalogoRepuestos[1, fila].Value.ToString(), Convert.ToInt32(this.dGVCatalogoRepuestos[2, fila].Value.ToString()),
+            //                             Convert.ToDouble(this.dGVCatalogoRepuestos[3, fila].Value.ToString()));
+
+            //    FrmRegistroDeCatalogoDeRepuestos oFrm = new FrmRegistroDeCatalogoDeRepuestos(oCatalogoRepuestoE);
+            //    oFrm.ShowDialog();
+            //    if (oFrm.aceptar)
+            //    {
+
+            //        if (oCatalogoRepuestoD.modificarCatalogoRepuesto(oFrm.oCatalogoRepuestoE, Convert.ToInt32(this.dGVCatalogoRepuestos[0, fila].Value.ToString()));
+            //        {
+            //            this.CargarDGview();
+            //            MessageBox.Show("Marca actualizada");
+            //        }
+
+            //    }
+
+            //}
+        }
+
+        private void BtnBorrar_Click(object sender, EventArgs e)
+        {
+            //if (this.dGVCatalogoRepuestos.Rows.Count > 0)
+            //{
+            //    DialogResult respuesta = MessageBox.Show("¿Está seguro de borrar?",
+            //                                             "Error",
+            //                                              MessageBoxButtons.YesNo,
+            //                                              MessageBoxIcon.Question);
+            //    if (respuesta == DialogResult.Yes)
+            //    {
+
+            //        int fila = this.dGVCatalogoRepuestos.CurrentRow.Index;
+
+            //        MarcaE oMarcaE = new MarcaE(Convert.ToInt32(this.dGVCatalogoRepuestos[0, fila].Value.ToString()),
+            //                                 this.dGVCatalogoRepuestos[1, fila].Value.ToString());
+
+
+            //        if (oCatalogoRepuestoD.borrarCatalogoRepuesto(oCatalogoRepuestoE))
+            //        {
+            //            this.CargarDGview();
+            //            MessageBox.Show("Marca borrada");
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Error borrando el marca: " +
+            //                       oCatalogoRepuestoD.ErrorMsg, "Error",
+            //                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        }
+            //    }
+            //}
+        }
+
+        private void BtnActualizar_Click(object sender, EventArgs e)
+        {
+            this.CargarDGview();
         }
     }
 }
--- ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmRegistroDeCatalogoDeRepuestos.cs
+++ ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmRegistroDeCatalogoDeRepuestos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logica;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,41 @@ namespace ProyectoPrograWilmerAndSteven.Vista
 {
     public partial class FrmRegistroDeCatalogoDeRepuestos : Form
     {
+        public bool aceptar;
+        public CatalogoRepuestoE oCatalogoRepuestoE;
         public FrmRegistroDeCatalogoDeRepuestos()
         {
             InitializeComponent();
         }
 
-        private void textAño_TextChanged(object sender, EventArgs e)
+        public FrmRegistroDeCatalogoDeRepuestos(CatalogoRepuestoE pCatalogoRepuestoE)
         {
+            InitializeComponent();
+            this.oCatalogoRepuestoE = pCatalogoRepuestoE;
+            this.txtId.Text = pCatalogoRepuestoE.IdCatalogoRepuesto.ToString();
+            this.txtNombreRepuesto.Text = pCatalogoRepuestoE.NombreDelRepuesto;
+            this.txtAnio.Text = pCatalogoRepuestoE.AnnoAlQuePertenece.ToString();
+            this.txtPrecio.Text = pCatalogoRepuestoE.Precio.ToString();
+        }
 
+        private void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (!(this.txtId.Text == "") && !(this.txtNombreRepuesto.Text == "") && !(this.txtAnio.Text == "") && !(this.txtPrecio.Text == ""))
+            {
+                this.oCatalogoRepuestoE = new CatalogoRepuestoE(Convert.ToInt32(this.txtId.Text), this.txtNombreRepuesto.Text,Convert.ToInt32(this.txtAnio.Text),
+                Convert.ToDouble(this.txtId.Text));
+                this.aceptar = true;
+                this.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar todos los datos");
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
         }
     }
 }
