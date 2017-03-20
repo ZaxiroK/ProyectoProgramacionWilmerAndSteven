c48e6d90cf750514cd74e92635c3111e128af936
Steven Viquez <sviquez48@gmail.com>
2017-03-19 21:09:51 -0600

    "FrmCatalogoReparacion2"

--- ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmCatalogoDeReparaciones2.Designer.cs
+++ ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmCatalogoDeReparaciones2.Designer.cs
@@ -28,19 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCatalogoDeReparaciones2));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BtnAgregar = new System.Windows.Forms.ToolStripButton();
+            this.BtnEditar = new System.Windows.Forms.ToolStripButton();
+            this.BtnBorrar = new System.Windows.Forms.ToolStripButton();
+            this.BtnActualizar = new System.Windows.Forms.ToolStripButton();
+            this.dGVCatalogoRepuestos = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Reparacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Anio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVCatalogoRepuestos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnAgregar,
+            this.BtnEditar,
+            this.BtnBorrar,
+            this.BtnActualizar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(573, 42);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Image = ((System.Drawing.Image)(resources.GetObject("BtnAgregar.Image")));
+            this.BtnAgregar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(53, 39);
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.Image = ((System.Drawing.Image)(resources.GetObject("BtnEditar.Image")));
+            this.BtnEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(41, 39);
+            this.BtnEditar.Text = "Editar";
+            this.BtnEditar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.BtnEditar.Click += new System.EventHandler(this.BtnEditar_Click);
+            // 
+            // BtnBorrar
+            // 
+            this.BtnBorrar.Image = ((System.Drawing.Image)(resources.GetObject("BtnBorrar.Image")));
+            this.BtnBorrar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnBorrar.Name = "BtnBorrar";
+            this.BtnBorrar.Size = new System.Drawing.Size(43, 39);
+            this.BtnBorrar.Text = "Borrar";
+            this.BtnBorrar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.BtnBorrar.Click += new System.EventHandler(this.BtnBorrar_Click);
+            // 
+            // BtnActualizar
+            // 
+            this.BtnActualizar.Image = ((System.Drawing.Image)(resources.GetObject("BtnActualizar.Image")));
+            this.BtnActualizar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnActualizar.Name = "BtnActualizar";
+            this.BtnActualizar.Size = new System.Drawing.Size(63, 39);
+            this.BtnActualizar.Text = "Actualizar";
+            this.BtnActualizar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.BtnActualizar.Click += new System.EventHandler(this.BtnActualizar_Click);
+            // 
+            // dGVCatalogoRepuestos
+            // 
+            this.dGVCatalogoRepuestos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVCatalogoRepuestos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Reparacion,
+            this.Anio,
+            this.Precio});
+            this.dGVCatalogoRepuestos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dGVCatalogoRepuestos.Location = new System.Drawing.Point(0, 42);
+            this.dGVCatalogoRepuestos.Name = "dGVCatalogoRepuestos";
+            this.dGVCatalogoRepuestos.Size = new System.Drawing.Size(573, 219);
+            this.dGVCatalogoRepuestos.TabIndex = 3;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "id_catalogoReparacion";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Reparacion
+            // 
+            this.Reparacion.DataPropertyName = "descripcion";
+            this.Reparacion.HeaderText = "Reparacion";
+            this.Reparacion.Name = "Reparacion";
+            // 
+            // Anio
+            // 
+            this.Anio.DataPropertyName = "horasReparacion";
+            this.Anio.HeaderText = "Horas";
+            this.Anio.Name = "Anio";
+            // 
+            // Precio
+            // 
+            this.Precio.DataPropertyName = "costoReparacion";
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
             // 
             // FrmCatalogoDeReparaciones2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(573, 261);
+            this.Controls.Add(this.dGVCatalogoRepuestos);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "FrmCatalogoDeReparaciones2";
             this.Text = "FrmCatalogoDeReparaciones2";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVCatalogoRepuestos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BtnAgregar;
+        private System.Windows.Forms.ToolStripButton BtnEditar;
+        private System.Windows.Forms.ToolStripButton BtnBorrar;
+        private System.Windows.Forms.ToolStripButton BtnActualizar;
+        private System.Windows.Forms.DataGridView dGVCatalogoRepuestos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reparacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Anio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
     }
 }
--- ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmCatalogoDeReparaciones2.cs
+++ ProyectoPrograWilmerAndSteven/ProyectoPrograWilmerAndSteven/Vista/FrmCatalogoDeReparaciones2.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Logica;
+using ProyectoPrograWilmerAndSteven.Datos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,106 @@ namespace ProyectoPrograWilmerAndSteven.Vista
 {
     public partial class FrmCatalogoDeReparaciones2 : Form
     {
+        CatalogoReparacionD oCatalogoReparacionD = new CatalogoReparacionD();
         public FrmCatalogoDeReparaciones2()
         {
             InitializeComponent();
+            this.CargarDGview();
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            FrmRegistroDeCatalgoDeReparaciones oFrm = new FrmRegistroDeCatalgoDeReparaciones();
+            oFrm.ShowDialog();
+
+
+            if (oFrm.aceptar)
+            {
+                if (oCatalogoReparacionD.agregarCatalogoReparacion(oFrm.oCatalogoE))
+                {
+                    this.CargarDGview();
+                    MessageBox.Show("Reparción agregada");
+                }
+                else
+                {
+                    MessageBox.Show("Error al agregar repuesto: " +
+                               oCatalogoReparacionD.ErrorMsg, "Error",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            if (this.dGVCatalogoRepuestos.Rows.Count > 0)
+            {
+
+                int fila = this.dGVCatalogoRepuestos.CurrentRow.Index;
+
+                CatalogoReparacionE oCatalogoReparacionE = new CatalogoReparacionE(Convert.ToInt32(this.dGVCatalogoRepuestos[0, fila].Value.ToString()),
+                                         this.dGVCatalogoRepuestos[1, fila].Value.ToString(), Convert.ToInt32(this.dGVCatalogoRepuestos[2, fila].Value.ToString()),
+                                         Convert.ToDouble(this.dGVCatalogoRepuestos[3, fila].Value.ToString()));
+
+                FrmRegistroDeCatalgoDeReparaciones oFrm = new FrmRegistroDeCatalgoDeReparaciones(oCatalogoReparacionE);
+                oFrm.ShowDialog();
+                if (oFrm.aceptar)
+                {
+
+                    if (oCatalogoReparacionD.modificarCatalogoReparacion(oFrm.oCatalogoE, Convert.ToInt32(this.dGVCatalogoRepuestos[0, fila].Value.ToString()))) ;
+                    {
+                        this.CargarDGview();
+                        MessageBox.Show("Reparacion actualizada");
+                    }
+
+                }
+
+            }
+        }
+
+        private void BtnBorrar_Click(object sender, EventArgs e)
+        {
+            if (this.dGVCatalogoRepuestos.Rows.Count > 0)
+            {
+                DialogResult respuesta = MessageBox.Show("¿Está seguro de borrar?",
+                                                         "Error",
+                                                          MessageBoxButtons.YesNo,
+                                                          MessageBoxIcon.Question);
+                if (respuesta == DialogResult.Yes)
+                {
+
+                    int fila = this.dGVCatalogoRepuestos.CurrentRow.Index;
+
+                    CatalogoReparacionE oCatalogoReparacionE = new CatalogoReparacionE(Convert.ToInt32(this.dGVCatalogoRepuestos[0, fila].Value.ToString()),
+                                         this.dGVCatalogoRepuestos[1, fila].Value.ToString(), Convert.ToInt32(this.dGVCatalogoRepuestos[2, fila].Value.ToString()),
+                                         Convert.ToDouble(this.dGVCatalogoRepuestos[3, fila].Value.ToString()));
+
+                    if (oCatalogoReparacionD.borrarCatalogoReparacion(oCatalogoReparacionE))
+                    {
+                        this.CargarDGview();
+                        MessageBox.Show("Reparacion borrada");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error borrando el Reparación: " +
+                                   oCatalogoReparacionD.ErrorMsg, "Error",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void CargarDGview()
+        {
+            List<CatalogoReparacionE> catalogoRepuesto = oCatalogoReparacionD.obtenerCatalogoReaparaciones();
+            if (!oCatalogoReparacionD.Error)
+            {
+                this.dGVCatalogoRepuestos.DataSource = catalogoRepuesto;
+            }
+        }
+
+        private void BtnActualizar_Click(object sender, EventArgs e)
+        {
+            this.CargarDGview();
         }
     }
 }
